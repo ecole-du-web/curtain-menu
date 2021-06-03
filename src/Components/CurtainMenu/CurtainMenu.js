3092a6a30c61e91615237dff3495cf86609bda3a
Ziratsu <enzo.ustariz@hotmail.fr>
2021-06-03 10:57:41 +0200

    init

---
+++ src/Components/CurtainMenu/CurtainMenu.js
@@ -0,0 +1,50 @@
+import React, { useState, useEffect } from "react";
+import "./CurtainMenu.css";
+import Menu from "./menu.svg";
+
+export default function CurtainMenu() {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const [checkWidth, setCheckWidth] = useState(window.innerWidth);
+
+  const checkFunc = () => {
+    console.log(checkWidth);
+    setCheckWidth(window.innerWidth);
+  };
+
+  useEffect(() => {
+    window.addEventListener("resize", checkFunc);
+
+    return () => {
+      window.removeEventListener("resize", checkFunc);
+    };
+  }, []);
+
+  const toggleNavFunc = () => {
+    setToggleNav(!toggleNav);
+  };
+
+  return (
+    <>
+      {checkWidth < 900 && (
+        <button onClick={toggleNavFunc} className="floating-btn">
+          <img src={Menu} />
+        </button>
+      )}
+
+      <nav className={toggleNav ? "active" : ""}>
+
+        {checkWidth < 900 && (
+          <button 
+          onClick={toggleNavFunc} className="close-curtain">
+            X
+          </button>
+        )}
+
+        <a href="#">HOME</a>
+        <a href="#">SERVICES</a>
+        <a href="#">CONTACT</a>
+      </nav>
+    </>
+  );
+}
